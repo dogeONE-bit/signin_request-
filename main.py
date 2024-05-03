dbe50c73e04e4f213e9f72ec28c1fe8dd743673b
dogeONE-bit <83808696+dogeONE-bit@users.noreply.github.com>
2024-05-03 16:27:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,20 +11,24 @@ def ParseCookiestr(cookie_str):
     return cookielist
 
 
+# headers = {
+#     'authority': 'glados.rocks',
+#     'accept': 'application/json, text/plain, */*',
+#     'accept-language': 'zh-CN,zh;q=0.9',
+#     'authorization': '74423511365535966705398523722820-720-1280',
+#     'content-type': 'application/json;charset=UTF-8',
+#     'origin': 'https://glados.network',
+#     'sec-ch-ua': '"Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"',
+#     'sec-ch-ua-mobile': '?0',
+#     'sec-ch-ua-platform': '"Windows"',
+#     'sec-fetch-dest': 'empty',
+#     'sec-fetch-mode': 'cors',
+#     'sec-fetch-site': 'same-origin',
+#     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+# }
+
 headers = {
-    'authority': 'glados.network',
-    'accept': 'application/json, text/plain, */*',
-    'accept-language': 'zh-CN,zh;q=0.9',
-    'authorization': '74423511365535966705398523722820-720-1280',
-    'content-type': 'application/json;charset=UTF-8',
-    'origin': 'https://glados.network',
-    'sec-ch-ua': '"Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-origin',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0',
 }
 
 json_data = {
